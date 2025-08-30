934de3de3b61e10a0a2715e5e45d38ef42705f77
Abhishek Adagale <126813011+abhishekadagale2509@users.noreply.github.com>
2025-08-31 00:29:22 +0530

    Add files via upload

---
+++ times.js
@@ -0,0 +1,45 @@
+/*
+Write a function that calculates the time (in seconds) it takes for the JS code to calculate sum from 1 to n, given n as the input.
+Try running it for
+1. Sum from 1-100
+2. Sum from 1-100000
+3. Sum from 1-1000000000
+Hint - use Date class exposed in JS
+There is no automated test for this one, this is more for you to understand time goes up as computation goes up
+*/
+
+function calculateTime(n) {
+    let x = 0;
+    for(let i=0; i<=n; i++) {
+        x += i;
+    }
+    return x;    
+}
+const beforeDate = new Date();
+const beforeTimeinMs = beforeDate.getTime();
+calculateTime(1000000000);
+const afterDate = new Date();
+const afterTimeinMs = afterDate.getTime();
+
+console.log(afterTimeinMs - beforeTimeinMs);
+
+/*function calculateTime(n) {
+    const start = new Date().getTime(); // in ms
+
+    let x = 0;
+    for (let i = 0; i <= n; i++) {
+        x += i;
+    }
+
+    const end = new Date().getTime(); // in ms
+    const timeTaken = (end - start) / 1000; // convert to seconds
+    console.log(`Time taken for n=${n}: ${timeTaken} seconds`);
+    
+    return x; // returning sum just in case you need it
+}
+
+// Try for different inputs
+calculateTime(100);          // very fast, close to 0
+calculateTime(100000);       // small but noticeable
+calculateTime(1000000000);   // will take a long time 🚀
+*/
